028a4a2031d17f292a40b51b3b179c4e078cad79
jeff.park <jeff.park@machbase.com>
2022-07-28 11:52:13 +0900

    ✨ 드롭다운 스토리 추가

---
+++ src/components/dropdown/Dropdown.stories.js
@@ -0,0 +1,17 @@
+import { CDropdown } from './index';
+
+export default {
+  title: 'KYUI/CDropdown',
+  component: CDropdown,
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { CDropdown },
+  template: '<c-dropdown v-bind="$props">dropdown</c-dropdown>',
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+    dropdownList: ['dropdown test','korea','USA'],
+}
