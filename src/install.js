5ee94a60a4cc577586a0f0674c8f292cd733124a
jeff.park <jeff.park@machbase.com>
2022-08-09 16:32:57 +0900

    🔧 npm publish 설정

---
+++ src/install.js
@@ -0,0 +1,42 @@
+import { CButton } from './components/button/index'
+import { CIcon } from './components/icon/index'
+import { CInput } from './components/input/index'
+import { CDropdown, CDropdownItem } from './components/dropdown/index'
+import { CCarousel } from './components/carousel/index'
+import { CRadio, CRadioGroup } from './components/radio/index'
+import { CLabel } from './components/label/index'
+import { CBreadcrumb, CBreadcrumbItem } from './components/breadcrumb/index'
+import { CPagination } from './components/pagination/index'
+import { CTooltip } from './components/tootip/index'
+import { CTag } from './components/tag/index'
+import { CSwitch } from './components/switch/index'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+const components = [
+    CButton,
+    CIcon,
+    CInput,
+    CDropdown,
+    CDropdownItem,
+    CCarousel,
+    CRadio,
+    CRadioGroup,
+    CLabel,
+    CBreadcrumb,
+    CBreadcrumbItem,
+    CPagination,
+    CTooltip,
+    CTag,
+    CSwitch,
+]
+
+const kyui = {
+    install(Vue) {
+        components.map(component => {
+            Vue.component(component.name, component)
+        })
+        Vue.component('f-icon', FontAwesomeIcon)
+    }
+};
+
+export default kyui;
