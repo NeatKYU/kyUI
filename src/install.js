a05d55180318021916d8f445d1c3e2447a48146f
jeff.park <jeff.park@machbase.com>
2023-01-26 15:23:20 +0900

    Revert "🔧 vue3 지원 테스트"
    This reverts commit 5845c3769e523e366d8c3a966aeaef1e904ff78c.

--- src/install.js
+++ src/install.js
@@ -1,5 +1,3 @@
-import { App } from 'vue';
-
 import { CButton } from './components/button/index'
 import { CIcon } from './components/icon/index'
 import { CInput } from './components/input/index'
@@ -14,6 +12,8 @@ import { CTag } from './components/tag/index'
 import { CSwitch } from './components/switch/index'
 import { CTable } from './components/table/index'
 import { CModal} from './components/modal/index'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 const components = [
     CButton,
@@ -36,19 +36,12 @@ const components = [
 ]
 
 const kyui = {
-    install(Vue: any) {
+    install(Vue) {
         components.map(component => {
             Vue.component(component.name, component)
         })
+        Vue.component('f-icon', FontAwesomeIcon)
     }
 };
-
-export const kyui3 = {
-    install(app: App) {
-        components.map(component => {
-            app.component(component.name, component)
-        })
-    }
-}
 
 export default kyui;
